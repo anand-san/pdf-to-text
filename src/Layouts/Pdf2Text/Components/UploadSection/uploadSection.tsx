c216ddd797665dd2d3e46eae262fdb31f9df8e05
Anand Sandilya <seanjin17@gmail.com>
2022-02-06 22:00:25 +0530

    feat: use mui buttons for upload/convert buttons

--- src/Layouts/Pdf2Text/Components/UploadSection/uploadSection.tsx
+++ src/Layouts/Pdf2Text/Components/UploadSection/uploadSection.tsx
@@ -1,6 +1,9 @@
 import './uploadFile.css'
 import { useDropzone, FileWithPath } from 'react-dropzone';
 import PDFToText from './assets/pdftotext.png'
+import Button from '@mui/material/Button';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import LoopIcon from '@mui/icons-material/Loop';
 
 export const UploadSection = () => {
     const {acceptedFiles, fileRejections, getRootProps, getInputProps} = useDropzone({
@@ -22,19 +25,31 @@ export const UploadSection = () => {
                         <input {...getInputProps()} />
                         <img src={PDFToText} alt={'pdftotext'} height={110} width={80}/>
                         <div className='uploadSection__content_actions'>
-                            <h2>Drop your file here or</h2>
-                            <button>Choose File</button>
+                            <h2>Drop your <strong>PDF</strong> file here or</h2>
+                            <Button startIcon={<UploadFileIcon />}>
+                                Choose File
+                            </Button>
                         </div>
-
                     </div>
                 </div>
                 {
                     !!files.length &&
                     <aside>
-                        <h4>Files</h4>
+                        <h4>Selected File</h4>
                         <ul>{files}</ul>
                     </aside>
                 }
+
+                {
+                    !!fileRejections.length && <p>Couldn't upload last file</p>
+                }
+
+            </div>
+
+            <div className='uploadSection__convertContainer'>
+                <Button className='uploadSection__convertButton' variant={'contained'} color='success' disabled={!files.length} startIcon={<LoopIcon />}>
+                    Convert
+                </Button>
             </div>
         </div>
     )
