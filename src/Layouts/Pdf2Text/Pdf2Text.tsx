905d7b32c580eea785d56ce9246fa3f7ad33e58d
Anand Sandilya <seanjin17@gmail.com>
2022-02-08 16:34:40 +0530

    feat: render ParsedTextArea

--- src/Layouts/Pdf2Text/Pdf2Text.tsx
+++ src/Layouts/Pdf2Text/Pdf2Text.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { UploadSection } from "./Components/UploadSection";
 import { HeadingSection } from "./Components/HeadingSection";
 import './Pdf2Text.css'
+import { ParsedTextArea } from "./Components/ParsedTextArea";
+import { ParsedPDFData } from "./";
 
 export const Pdf2Text = () => {
-  return (
+    const [parsedFileData, setParsedFileData] = React.useState<ParsedPDFData>()
+
+    return (
       <div className='pdfToText'>
           <HeadingSection />
-          <UploadSection />
+          <UploadSection setParsedFileData={setParsedFileData} />
+          <ParsedTextArea pdfData={parsedFileData}/>
       </div>
   );
 }
