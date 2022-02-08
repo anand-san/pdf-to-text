75004994882a02db5d7398547be5d2e7086d7f68
Anand Sandilya <seanjin17@gmail.com>
2022-02-08 17:00:02 +0530

    feat: add button to re-upload pdf in PDFParsedText page

--- src/Layouts/Pdf2Text/Components/ParsedTextArea/ParsedTextArea.tsx
+++ src/Layouts/Pdf2Text/Components/ParsedTextArea/ParsedTextArea.tsx
@@ -2,16 +2,28 @@ import TextField from "@mui/material/TextField";
 import React from "react";
 import './ParsedTextArea.css'
 import { ParsedTextAreaProps } from "./types";
+import Button from "@mui/material/Button";
 
-export const ParsedTextArea = ({ pdfData }: ParsedTextAreaProps) => <div className='parsedTextArea__container'>
-    <TextField
-        id="outlined-multiline-flexible"
-        label="Parsed Text"
-        multiline
-        maxRows={20}
-        minRows={8}
-        value={pdfData?.text}
-        fullWidth
-    />
-</div>
+export const ParsedTextArea = ({ pdfData, setPdfData }: ParsedTextAreaProps) =>
+    <div className='parsedTextArea__container'>
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Parsed Text"
+            multiline
+            maxRows={20}
+            minRows={8}
+            value={pdfData?.text}
+            fullWidth
+        />
+
+        <Button
+            className='parsedTextArea__button'
+            variant={'contained'}
+            color='success'
+            onClick={() => setPdfData(undefined)}
+        >
+            Convert Another
+        </Button>
+
+    </div>
 
