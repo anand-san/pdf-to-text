37c2b8c11c40d1cac62901c6b5c7ad7f1c7263e5
Anand Sandilya <seanjin17@gmail.com>
2022-02-08 16:42:05 +0530

    refactor: consistent naming

--- src/Layouts/Pdf2Text/Components/UploadSection/types.ts
+++ src/Layouts/Pdf2Text/Components/UploadSection/types.ts
@@ -2,5 +2,5 @@ import React from "react";
 import { ParsedPDFData } from "../../";
 
 export interface UploadSectionProps {
-    setParsedFileData: React.Dispatch<React.SetStateAction<ParsedPDFData | undefined>>
+    setPdfData: React.Dispatch<React.SetStateAction<ParsedPDFData | undefined>>
 }
