e51b7a13bc3f535fd23f96249ec7af120441c728
Anand Sandilya <anand.sandilya@mieterengel.de>
2022-02-08 23:42:54 +0530

    feat: add download text as file button

--- src/Layouts/Pdf2Text/Components/ParsedTextArea/ParsedTextArea.tsx
+++ src/Layouts/Pdf2Text/Components/ParsedTextArea/ParsedTextArea.tsx
@@ -2,19 +2,49 @@ import React from "react";
 import './ParsedTextArea.css'
 import { ParsedTextAreaProps } from "./types";
 import Button from "@mui/material/Button";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-export const ParsedTextArea = ({ pdfData, setPdfData }: ParsedTextAreaProps) =>
-    <div className='parsedTextArea__container'>
+export const ParsedTextArea = ({ pdfData, setPdfData }: ParsedTextAreaProps) => {
+    const downloadTxtFile = () => {
+        if(!pdfData?.text)
+            return
+
+        const element = document.createElement("a");
+        const file = new Blob([pdfData.text],
+            {type: 'text/plain;charset=utf-8'});
+        element.href = URL.createObjectURL(file);
+        element.download = "pdf2text-converted.txt";
+        document.body.appendChild(element);
+        element.click();
+        element.remove()
+    }
+
+    return <div className='parsedTextArea__container'>
         <textarea value={pdfData?.text} disabled className='parsedTextArea__textArea' placeholder='Parsed Text'/>
 
-        <Button
-            className='parsedTextArea__button'
-            variant={'contained'}
-            color='success'
-            onClick={() => setPdfData(undefined)}
-        >
-            Convert Another
-        </Button>
+        <div className='parsedTextArea__actionButtonsContainer'>
+            <Button
+                className='parsedTextArea__button'
+                variant={'contained'}
+                color='primary'
+                onClick={() => setPdfData(undefined)}
+                startIcon={<ArrowBackIcon />}
+            >
+                Convert Another
+            </Button>
 
+            <Button
+                className='parsedTextArea__button'
+                variant={'contained'}
+                color='success'
+                onClick={downloadTxtFile}
+                startIcon={<FileDownloadIcon />}
+            >
+                Download Text File
+            </Button>
+        </div>
     </div>
+}
+
 
