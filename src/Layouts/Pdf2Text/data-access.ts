e13c2fa8d9a4c6821701d06cb4ebad2218c96fab
Anand Sandilya <seanjin17@gmail.com>
2022-02-08 21:26:17 +0530

    feat: move api base url to .env

--- src/Layouts/Pdf2Text/data-access.ts
+++ src/Layouts/Pdf2Text/data-access.ts
@@ -1,13 +1,11 @@
 import axios from 'axios'
 import { ParsedPDFData } from "./";
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://6mosdmxik3.execute-api.us-east-1.amazonaws.com/dev/' : 'http://localhost:4000/'
-
 export const uploadFile = async (file: File): Promise<ParsedPDFData> => {
     let formData = new FormData();
     formData.append("file", file);
 
-    const response = await axios.post(BASE_URL + 'upload', formData )
+    const response = await axios.post(process.env.REACT_APP_API_BASE_URL + 'uploadPdf', formData )
 
     return response.data.pdfData
 };
